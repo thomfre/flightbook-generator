5331087cf53f03e80f212daf137fa156f7ff07c8
thomfre <10846906+thomfre@users.noreply.github.com>
2021-06-09 00:24:11 +0200

    Added missing JsonIgnore

--- Flightbook.Generator/Models/Tracklogs/GpxTrack.cs
+++ Flightbook.Generator/Models/Tracklogs/GpxTrack.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace Flightbook.Generator.Models.Tracklogs
@@ -6,6 +7,7 @@ namespace Flightbook.Generator.Models.Tracklogs
     public class GpxTrack
     {
         public string Date { get; set; }
+        [JsonIgnore]
         public DateTime DateTime { get; set; }
         public string Name { get; set; }
         public string Aircraft { get; set; }
