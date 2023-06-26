f405e1ec6d45592616767ddc7d1d4057de20202d
thomfre <10846906+thomfre@users.noreply.github.com>
2023-06-26 23:17:12 +0200

    Replace Norwegian characters with alternatives

--- Flightbook.Generator/Export/FlightbookJsonExporter.cs
+++ Flightbook.Generator/Export/FlightbookJsonExporter.cs
@@ -99,7 +99,7 @@ namespace Flightbook.Generator.Export
 
         private static string GetAircraftOperatorPicture(string aircraftOperator)
         {
-            string filename = aircraftOperator.Replace(" ", "-").ToLowerInvariant();
+            string filename = aircraftOperator.Replace(" ", "-").ToLowerInvariant().Replace("ø", "o").Replace("å", "a").Replace("æ", "ae");
             filename = string.Join("_", filename.Split(Path.GetInvalidFileNameChars(), StringSplitOptions.RemoveEmptyEntries)).TrimEnd('.');
 
             return !File.Exists($@"config\operators\{filename}.png") ? null : $"/operators/{filename}.png";
