81f1bca5192f4e80287b6338d56e04edcc04933e
thomfre <10846906+thomfre@users.noreply.github.com>
2023-09-20 17:52:50 +0200

    Better heatmap data

--- Flightbook.Generator/Export/HeatmapExporter.cs
+++ Flightbook.Generator/Export/HeatmapExporter.cs
@@ -18,8 +18,12 @@ namespace Flightbook.Generator.Export
 
         public string CreateHeatmapFile(List<GpxTrack> tracks)
         {
-            var groupedPoints = tracks.Select(t => t.GeoJson as LineString).SelectMany(x => x.Coordinates).GroupBy(x => (Math.Round(x.Latitude, CoordinateRounding), Math.Round(x.Longitude, CoordinateRounding)))
-                .Select(g => new {Latitude = g.Key.Item1, Longitude = g.Key.Item2, Count = g.Count()});
+            var groupedPoints = tracks.GroupBy(t => t.DateTime.Year).Select(g => new
+            {
+                Year = g.Key, Points = g.Select(t => t.GeoJson as LineString).SelectMany(x => x.Coordinates).GroupBy(x => (Math.Round(x.Latitude, CoordinateRounding), Math.Round(x.Longitude, CoordinateRounding)))
+                    .Select(g => new[] {g.Key.Item1, g.Key.Item2, g.Count()})
+            });
+
 
             return JsonConvert.SerializeObject(groupedPoints);
         }
